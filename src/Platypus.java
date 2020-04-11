45e267af337c7ab8151883316d460eccb94d6485
JoshuaJasso1 <joshuajasso1@gmail.com>
2020-04-11 11:28:41 -0700

    Finished Intro to Constructors

---
+++ src/Platypus.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Platypus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+	public void sayHi() {
+		String name = JOptionPane.showInputDialog("What is your name?");
+		System.out.println("The platypus "+ name + " is smarter than your average platypus.");
+	}
+}
