d77f3396a86b1b649cb583804c0b801408708353
JoshuaJasso1 <joshuajasso1@gmail.com>
2020-04-27 06:37:16 -0700

    finished race car runner

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -11,14 +11,19 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+		RaceCar raceCar = new RaceCar("Ferrari", 5);
 		// 2. Print the RaceCar's position in the race
-
+		raceCar.getPositionInRace();
 		// 3. Crash the RaceCar
-		
+		raceCar.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+		raceCar.isDamaged();
+		raceCar.pit();
 		// 5. Help the car move into first place.
+		for(int i= 0; i<= 7; i++){
+			raceCar.overtake();
 
+		}
+		
 	}
 }
