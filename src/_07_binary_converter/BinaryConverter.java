9240371192df465ef198e001bf44fd6aa9bcea49
JoshuaJasso1 <joshuajasso1@gmail.com>
2020-06-20 11:28:07 -0700

    Finished binary converter

--- src/_07_binary_converter/BinaryConverter.java
+++ src/_07_binary_converter/BinaryConverter.java
@@ -5,56 +5,87 @@ import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class BinaryConverter implements MouseListener{
+public class BinaryConverter implements MouseListener {
+	JTextField jtextfield;
+	JLabel label;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BinaryConverter bC = new BinaryConverter();
 		bC.drawBinaryConverter();
-		
+
 	}
+
 	public void drawBinaryConverter() {
-		JFrame frame = new JFrame();
+		JFrame frame = new JFrame("Convert 8 Bits of Binary to ASCII");
 		JPanel panel = new JPanel();
-		JTextField jtextfield = new JTextField(20);
+		jtextfield = new JTextField(20);
 		panel.add(jtextfield);
 		frame.add(panel);
-		JButton button = new JButton();
+		JButton button = new JButton("Convert");
+		label = new JLabel(" ");
 		panel.add(button);
+		panel.add(label);
 		button.addMouseListener(this);
-		
+		frame.pack();
+		frame.setVisible(true);
 	}
+
+	String convert(String input) {
+		if (input.length() != 8) {
+			JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+			return "-";
+		}
+		String binary = "[0-1]+";// must contain numbers in the given range
+		if (!input.matches(binary)) {
+			JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+			return "-";
+		}
+		try {
+			int asciiValue = Integer.parseInt(input, 2);
+			char theLetter = (char) asciiValue;
+			return "" + theLetter;
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+			return "-";
+		}
+	}
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		String input = jtextfield.getText();
+
+		String ascii = convert(input);
+		label.setText(ascii);
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
